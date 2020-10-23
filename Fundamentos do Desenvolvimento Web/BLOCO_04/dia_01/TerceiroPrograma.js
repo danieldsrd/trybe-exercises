807a4d4aad294e22df215e8df19659246a9bdcd1
Daniel <danieldsrd@gmail.com>
2020-10-23 17:26:47 -0300

    Terceiro Programa JS

---
+++ Fundamentos do Desenvolvimento Web/BLOCO_04/dia_01/TerceiroPrograma.js
@@ -0,0 +1,11 @@
+let valorX = 20;
+let valorY = 90;
+let valorZ = 70;
+
+if (valorX > valorY && valorX > valorZ) {
+    console.log("O " + valorX + ", é o maior número")
+} else if (valorY > valorZ && valorY > valorX) { 
+    console.log("O " + valorY + ", é o maior número")
+} else {
+    console.log("O " + valorZ + ", é o maior número")
+}
