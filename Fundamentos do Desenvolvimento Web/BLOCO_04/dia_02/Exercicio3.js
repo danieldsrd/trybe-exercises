bd24f220968b3fa8a56f1b112ec4323221a60e6a
Daniel <danieldsrd@gmail.com>
2020-10-26 18:27:16 -0300

    Exericios do dia 2

---
+++ Fundamentos do Desenvolvimento Web/BLOCO_04/dia_02/Exercicio3.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somatorio = 0;
+
+for (let i=0; i < numbers.length; i++) {
+    somatorio = somatorio + numbers[i];    
+}
+let media = somatorio/numbers.length;
+console.log("A media aritmetica é "+ media);
