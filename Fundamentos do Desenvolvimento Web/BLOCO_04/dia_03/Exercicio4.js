f6d07392e937a2f8020949eb117833526815839f
Daniel <danieldsrd@gmail.com>
2020-10-27 18:09:17 -0300

    Exercicios 4 e 5 das piramides

---
+++ Fundamentos do Desenvolvimento Web/BLOCO_04/dia_03/Exercicio4.js
@@ -0,0 +1,20 @@
+let n = 11;
+let sum = " ";
+let asterisco = "*";
+let espaco = " ";
+
+if (n > 1) {
+    for (i=0; i < n; i +=2) {
+        for (let j=0; j < n; j +=1) {
+            if ((j >= ((n-i-1)/2)) && (j <= ((n+i-1)/2)))  {
+                sum += asterisco;           
+            } else {
+                sum += espaco;                
+            }          
+        }   
+        console.log(sum);
+        sum = " ";
+    }
+} else {
+    console.log("Valor inválido!");
+}
