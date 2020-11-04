0391d15cd5286689b2c1dac3acb0060cc3771a6e
Daniel <danieldsrd@gmail.com>
2020-11-04 16:45:09 -0300

    Exercicio 1!

---
+++ Fundamentos do Desenvolvimento Web/BLOCO_05/dia_03/script.js
@@ -0,0 +1,45 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+  // EXERCICIO 1
+  
+  
+  let ulList = document.querySelector('#day');
+ 
+
+  function createDays () {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const ulList = document.querySelector('#days');
+    
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const daysList = dezDaysList[index];        
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = daysList;
+        dayListItem.className = 'day';
+        if (dayListItem.innerHTML.includes('24') || dayListItem.innerHTML.includes('31')) {
+            dayListItem.className = 'day holiday';
+        }
+        if (dayListItem.innerHTML === '4' || dayListItem.innerHTML.includes('11') || dayListItem.innerHTML.includes('18')) {
+            dayListItem.className = 'day friday';
+        }
+        if (dayListItem.innerHTML.includes('25')) {
+            dayListItem.className = 'day friday holiday';
+        }
+    
+        ulList.appendChild(dayListItem);
+      };
+  };
+createDays();
