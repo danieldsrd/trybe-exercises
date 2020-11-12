c4fa1db2228816b22fdfa51ab951945aafbd72cc
Daniel <danieldsrd@gmail.com>
2020-11-12 19:34:36 -0300

    Finalizando exercicio 6.1 só falta estilizar no CSS

--- Fundamentos do Desenvolvimento Web/BLOCO_06/dia_01/script.js
+++ Fundamentos do Desenvolvimento Web/BLOCO_06/dia_01/script.js
@@ -5,7 +5,7 @@ function createOptionsState() {
     for (let index = 0; index < states.length; index++) {
         let optionState = document.createElement('option');
         optionState.textContent = states[index];
-        optionState.value = abrevStates[index];
+        optionState.value = states[index];
         document.querySelector('#input-estado').appendChild(optionState);
     }
 }
@@ -13,6 +13,9 @@ createOptionsState();
 
 document.querySelector('#criar-cv').addEventListener('click', function (e){
     e.preventDefault();
+    while(document.querySelector('#cv-montado').firstChild) {
+        document.querySelector('#cv-montado').removeChild(document.querySelector('#cv-montado').firstChild);
+    }
     let pName = document.createElement('p');
     pName.textContent = "Nome Completo: "+document.querySelector('#nome-completo').value;
     document.querySelector('#cv-montado').appendChild(pName);
@@ -29,18 +32,44 @@ document.querySelector('#criar-cv').addEventListener('click', function (e){
     pCity.textContent = "Estado: "+document.querySelector('#input-cidade').value;
     document.querySelector('#cv-montado').appendChild(pCity);
     let pState = document.createElement('p');
-    pState.textContent = "Estado: "+document.querySelector('#input-estado').value;
+    pState.textContent = "Estado: "+document.querySelector('#input-estado').value;   
     document.querySelector('#cv-montado').appendChild(pState);
+    if (document.querySelector('#moradia-1').checked) {
+        let pHome = document.createElement('p');
+        pHome.textContent = "Moradia: "+document.querySelector('#moradia-1').value;   
+        document.querySelector('#cv-montado').appendChild(pHome);
+    }
+    if (document.querySelector('#moradia-2').checked) {
+        let pHome = document.createElement('p');
+        pHome.textContent = "Moradia: "+document.querySelector('#moradia-2').value;   
+        document.querySelector('#cv-montado').appendChild(pHome);
+    }
     let pWork = document.createElement('p');
-    pWork.textContent = "Experiência Profissional: "+document.querySelector('#textarea-1').value
+    pWork.textContent = "Experiência Profissional: "+document.querySelector('#textarea-1').value;
     document.querySelector('#cv-montado').appendChild(pWork);
     let pRole = document.createElement('p');
-    pRole.textContent = "Cargo: "+document.querySelector('#input-cargo').value
+    pRole.textContent = "Cargo: "+document.querySelector('#input-cargo').value;
     document.querySelector('#cv-montado').appendChild(pRole);
     let pJobFunction = document.createElement('p');
-    pJobFunction.textContent = "Descrição do cargo: "+document.querySelector('#input-descr').value
+    pJobFunction.textContent = "Descrição do cargo: "+document.querySelector('#input-descr').value;
     document.querySelector('#cv-montado').appendChild(pJobFunction);
     let pInitialDate= document.createElement('p');
-    pInitialDate.textContent = "Data de Inicío: "+document.querySelector(' #input-data').value
+    pInitialDate.textContent = "Data de Inicío: "+document.querySelector(' #input-data').value;
     document.querySelector('#cv-montado').appendChild(pInitialDate);
+});
+
+document.querySelector('#limpar').addEventListener('click', function(){
+    while(document.querySelector('#cv-montado').firstChild) {
+        document.querySelector('#cv-montado').removeChild(document.querySelector('#cv-montado').firstChild);
+    }
+    document.querySelector('#nome-completo').value = '';
+    document.querySelector('#input-email').value = '';
+    document.querySelector('#input-cpf').value = '';
+    document.querySelector('#input-endereco').value = '';
+    document.querySelector('#input-cidade').value = '';
+    document.querySelector('#input-estado').value = '';
+    document.querySelector('#textarea-1').value = '';
+    document.querySelector('#input-cargo').value = '';
+    document.querySelector('#input-descr').value = '';
+    document.querySelector(' #input-data').value = '';
 });
