057f26ac5433fa8a7b0854351ee7ee2bdce35fc7
Daniel <danieldsrd@gmail.com>
2020-11-25 19:57:55 -0300

    Bonus 5 - Verificar senha

---
+++ Fundamentos do Desenvolvimento Web/BLOCO_07/dia_03/Bonus5.js
@@ -0,0 +1,38 @@
+function PasswordVerifier (password) {
+  const regex = /[0-9]/
+  const upper = /[A-Z]/
+  const lower = /[a-z]/
+  if (password === null) {
+    throw new Error('Password should not be null');
+  }
+  if (password.length <= 8) {
+    throw new Error('Password should be longer than 8 characters'); 
+  }
+  if (!upper.test(password)) {
+    throw new Error('Password should have one uppercase letter at least');
+  }
+  if (!lower.test(password)) {
+    throw new Error('Password should have one lowercase letter at least');
+  }
+  if (!regex.test(password)) {
+    throw new Error('Password should have one number at least');
+  }
+  return ('Password valid!');
+}
+
+console.log(PasswordVerifier('DanielRodrigues1'));
+
+const assert = require('assert');
+
+assert.throws(() => {
+  PasswordVerifier('AHhausahuAHAhsdausdh');
+}, /^Error: Password should have one number at least$/);
+assert.throws(() => {
+  PasswordVerifier('aaaaa');
+}, /^Error: Password should be longer than 8 characters$/);
+assert.throws(() => {
+  PasswordVerifier('aaaaaaaaa');
+}, /^Error: Password should have one uppercase letter at least$/);
+assert.throws(() => {
+  PasswordVerifier('AAAAAAAAAAA');
+}, /^Error: Password should have one lowercase letter at least$/);
