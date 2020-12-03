f362c780d292a60642077c3b4d0d37fb27ffa5e6
Daniel <danieldsrd@gmail.com>
2020-12-03 16:06:58 -0300

    Exercicio 8

--- Fundamentos do Desenvolvimento Web/BLOCO_08/dia_04/Exercicio8.js
+++ Fundamentos do Desenvolvimento Web/BLOCO_08/dia_04/Exercicio8.js
@@ -1,12 +1,14 @@
 const assert = require('assert')
 
-const greet = (string1, string2) => {
-  if (string2 === undefined) {
-    return `Hi ${string1}`;
-  } else {
-    return `${string2} ${string1}`;
-  }
-}
+// const greet = (string1, string2) => {
+//   if (string2 === undefined) {
+//     return `Hi ${string1}`;
+//   } else {
+//     return `${string2} ${string1}`;
+//   }
+// }
+// OUTRA ALTERNATIVA MAIS SIMPLES
+const greet = (string1, string2 = 'Hi') => `${string2} ${string1}`;
 
 assert.strictEqual(greet("John"), "Hi John")
 assert.strictEqual(greet("John", "Good morning"), "Good morning John")
