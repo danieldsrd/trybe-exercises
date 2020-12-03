87e92cb9f549f2407c32f6a5bea9f7aeda487458
Daniel <danieldsrd@gmail.com>
2020-12-03 16:59:16 -0300

    Refatorando Exercicio 2

--- Fundamentos do Desenvolvimento Web/BLOCO_08/dia_04/Exercicio2.js
+++ Fundamentos do Desenvolvimento Web/BLOCO_08/dia_04/Exercicio2.js
@@ -1,12 +1,6 @@
 const assert = require('assert')
 
-const sum = (...args) => {
-  if (args.length === 0) {
-    return 0;
-  } else {
-    return args.reduce((acc, curr) => acc + curr, 0);
-  }
-}
+const sum = (...args) => (args.length === 0) ? 0 : args.reduce((acc, curr) => acc + curr, 0);
 
 assert.strictEqual(sum(), 0)
 assert.strictEqual(sum(1), 1)
