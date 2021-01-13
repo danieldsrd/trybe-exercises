668151aca14cf5825d5cb19d6a385fe11a35cd0d
Daniel <danieldsrd@gmail.com>
2021-01-13 12:49:30 -0300

    exercicio

---
+++ Desenvolvimento Front-end/BLOCO_11/dia_02/my-pokedex/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import pokemons from './data';
+import Pokedex from './Components/Pokedex'
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <main className="pokemon-card">
+        <Pokedex pokemons={pokemons} />
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ Desenvolvimento Front-end/BLOCO_11/dia_02/my-pokedex/src/Components/Pokemon.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Pokemon extends React.Component {
+  render() {
+    const { name, type, averageWeight, image } = this.props.pokemon;
+
+    return (
+      <div>             
+        <span>{name}</span>
+        <span>{type}</span><br></br>        
+        <span>{averageWeight.value} {averageWeight.measurementUnit}</span>
+        <div>
+          <img src={image} alt="pokemon" />
+        </div>        
+      </div>
+    )
+  }
+}
+
+Pokemon.propTypes = {
+  type: PropTypes.string
+}
+
+export default Pokemon;
+
